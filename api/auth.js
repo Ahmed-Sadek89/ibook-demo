5885194eb1656025a21fcc617ac6f83c5ef266aa
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2025-04-06 19:45:52 +0200

    fix

--- api/auth.js
+++ api/auth.js
@@ -1,7 +1,17 @@
 'use client'
 import Cookies from 'universal-cookie'
 import { apiClient } from '../Utils/axios'
-
+export const login = async (type, body) => {
+  try {
+    const response = await apiClient.post(
+      `/crm/${type}/auth/login?lang=ar`,
+      body
+    )
+    return response.data
+  } catch (error) {
+    console.error(`Unexpected Error: ${error}`)
+  }
+}
 export const logout = async () => {
   const cookie = new Cookies()
   const token = cookie.get('ibook-auth')
